7ea9b02b089ce6f8b18d2b17376d8944eaf94895
tobilobaowolabi <tobiloba.0@gameporte.com>
2023-07-05 00:01:42 +0100

    initial commit

---
+++ src/main/java/com/example/klashabackendassessment/KlashaBackendAssessmentApplication.java
@@ -0,0 +1,12 @@
+package com.example.klashabackendassessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KlashaBackendAssessmentApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(KlashaBackendAssessmentApplication.class, args);
+  }
+}
---
+++ src/test/java/com/example/klashabackendassessment/KlashaBackendAssessmentApplicationTests.java
@@ -0,0 +1,11 @@
+package com.example.klashabackendassessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KlashaBackendAssessmentApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
